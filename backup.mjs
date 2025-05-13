a56d9ea3bb34d8d6dd0a42d948e7e27d3b4eb66b
SdwStorm <sdwstorm@gmail.com>
2025-05-13 22:25:13 +0300

    Added logger

--- backup.mjs
+++ backup.mjs
@@ -3,12 +3,15 @@ import { delay } from './utility/delay.mjs';
 import { readJSON } from './utility/readJson.mjs';
 import { getDate } from './utility/formattedDate.mjs';
 import checkFolder  from './utility/folder.mjs';
+import logger from './logger.mjs';
 
 const ssh = new NodeSSH();
 const config = await readJSON('config.json');
 
 export  async function saveBackup(address, name, key, sshport) {
-    console.log(`Connecting to ${address}...`);
+    console.log(`Connecting to ${address}`);
+    await logger.addLine(`Connecting to ${address}`);
+
     try {
         await ssh.connect({
             host: address, 
@@ -18,15 +21,17 @@ export  async function saveBackup(address, name, key, sshport) {
         });
 
         await ssh.execCommand('/system backup save name=backup');
-        console.log('Creating backup file...')
+        console.log('Creating backup file')
+        await logger.addLine('Creating backup file')
         await delay(config.delays.creatingBackup); //даем роутеру время на создание бэкапа
 
         let date = getDate();
-        let folderName = `backups/${date}`;
+        let folderName = `./backups/${date}`;
         let backupName = `backup-${address}-${date}.backup`;
         await checkFolder(folderName);
 
-        console.log('Downloading backup file...');
+        console.log('Downloading backup file');
+        await logger.addLine('Downloading backup file');
 
         await ssh.getFile(
             `./${folderName}/${backupName}`,   // Локальный файл
@@ -34,11 +39,19 @@ export  async function saveBackup(address, name, key, sshport) {
         );
 
         await ssh.execCommand('/file remove backup.backup');
-        console.log(`File ${backupName} downloaded. Disconnecting...`);
+
+        console.log(`File ${backupName} downloaded. Disconnecting`);
+        await logger.addLine(`File ${backupName} downloaded. Disconnecting`);
+
         ssh.dispose();
-        console.log('Done');
+
+        console.log('Success\n');
+        await logger.addLine('Success', true);
+        logger.countSaved();
     } catch (err) {
-        console.error('Ошибка:', err.message);
+        console.error('Failed:', err.message);
+        await logger.addLine(`Failed: ${err.message}`, true);
         ssh.dispose();
+        logger.countFailed();
     }
 }
